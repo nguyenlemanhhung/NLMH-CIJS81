80bbd0c736fc5f7cc22811f22defb74cdb21d701
nguyenlemanhhung <github.nohssiw1905@gmail.com>
2023-03-31 19:59:10 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const apiKey = "5a32dde605721f1437d5908b546a5415";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [city, setCity] = useState(null);
+
+  const getData = (city) => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    )
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setData(data);
+      });
+  };
+  // useEffect(() => {
+  //   getData();
+
+  // }, []);
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setCity(value);
+  };
+  const handleSearchCity = () => {
+    console.log("onclick");
+    getData(city);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div>
+          <input
+            name="city"
+            type="text"
+            placeholder="Enter City"
+            onChange={handleChange}
+          />
+          <button type="button" onClick={handleSearchCity}>
+            Search
+          </button>
+        </div>
+        <div className="card-contaier">
+          <h2></h2>
+        </div>
+      </>
     </div>
   );
 }
